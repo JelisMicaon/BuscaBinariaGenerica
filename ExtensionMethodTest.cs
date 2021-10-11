2d027384d906b87e8e071cdfc0814e946fe9d1a8
JelisMicaon <49448382+JelisMicaon@users.noreply.github.com>
2021-10-11 17:27:41 -0300

    Add files via upload

---
+++ ExtensionMethodTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace UnitaryTests
+{
+    public class ExtensionMethodTest
+    {
+        [Fact(DisplayName = "Verificar retorno vazio de lista sem dados")]
+        public void VerificarRetornoVazioSemDados()
+            => Assert.Null(StaticsClass.ReturnListEmpty().BinarySearch<InfoList>("Id", 123, false));
+
+        [Fact(DisplayName = "Verificar retorno vazio de lista com dados")]
+        public void VerificarRetornoVazioComDados()
+            => Assert.Null(StaticsClass.ReturnListInfo().BinarySearch<InfoList>("Id", 999, true));
+
+        [Fact(DisplayName = "Verificar retorno item de lista com dados")]
+        public void VerificarRetornoItemComDados()
+            => Assert.NotNull(StaticsClass.ReturnListInfo().BinarySearch<InfoList>("Id", 123, true));
+
+        [Fact(DisplayName = "Verificar retorno vazio de lista sem dados e propriedade inexistente")]
+        public void VerificarRetornoPropriedadeInexistenteComDados()
+            => Assert.Null(StaticsClass.ReturnListEmpty().BinarySearch<InfoList>("Name", 123, false));
+
+        [Fact(DisplayName = "Verificar retorno vazio de lista com dados e propriedade inexistente")]
+        public void VerificarRetornoPropriedadeInexistenteSemDados()
+            => Assert.Null(StaticsClass.ReturnListInfo().BinarySearch<InfoList>("Name", 123, true));
+
+        [Fact(DisplayName = "Verificar retorno de lista ordenada por propriedade sem dados")]
+        public void VerificarRetornoListaOrdenadaSemDados()
+            => Assert.NotNull(StaticsClass.ReturnListEmpty().ListOrderByProperty<InfoList>("Id"));
+
+        [Fact(DisplayName = "Verificar retorno de lista ordenada por propriedade sem dados e propriedade inexistente")]
+        public void VerificarRetornoListaOrdenadaPropriedadeInexistenteSemDados()
+            => Assert.Null(StaticsClass.ReturnListEmpty().ListOrderByProperty<InfoList>("Name"));
+
+        [Fact(DisplayName = "Verificar retorno de lista ordenada por propriedade com dados e propriedade inexistente")]
+        public void VerificarRetornoListaOrdenadaPropriedadeInexistenteComDados()
+            => Assert.Null(StaticsClass.ReturnListInfo().ListOrderByProperty<InfoList>("Name"));
+
+        [Fact(DisplayName = "Verificar retorno de lista ordenada por propriedade com dados")]
+        public void VerificarRetornoListaOrdenadaComDados()
+        {
+            List<InfoList> listaRegistros = StaticsClass.ReturnListInfo();
+            Assert.Equal(listaRegistros.OrderBy(x => x.Id).ToList(), listaRegistros.ListOrderByProperty<InfoList>("Id"));
+        }
+
+        [Fact(DisplayName = "Verificar retorno de lista ordenada por propriedade com dados (retorno diferente)")]
+        public void VerificarRetornoListaOrdenadaPropriedadeComDados()
+        {
+            List<InfoList> listaRegistros = StaticsClass.ReturnListInfo();
+            Assert.NotEqual(listaRegistros, listaRegistros.ListOrderByProperty<InfoList>("Id"));
+        }
+    }
+}
---
+++ StaticsClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace UnitaryTests
+{
+    public static class StaticsClass
+    {
+        #region Algorithm Binary
+        public static List<T> ListOrderByProperty<T>(this List<T> list, string property)
+            => typeof(T).GetProperty(property) is null ? null : list.OrderBy(x => typeof(T).GetProperty(property)?.GetValue(x, null)).ToList();
+
+        public static T BinarySearch<T>(this List<T> list, string propertySearch, long valueSearch, bool orderList = false) where T : class
+        {
+            List<PropertyInfo> propriedades = typeof(T).GetProperties().ToList();
+            if (propriedades.Any(x => x.Name == propertySearch) && list.Count > 0)
+            {
+                if (orderList)
+                    list = list.ListOrderByProperty<T>(propertySearch);
+
+                int middle;
+                int start = 0;
+                int end = list.Count - 1;
+                long valueProperty;
+
+                do
+                {
+                    middle = (start + end) / 2;
+                    valueProperty = Convert.ToInt64(list.ElementAt(middle).GetType().GetProperty(propertySearch).GetValue(list.ElementAt(middle)));
+                    if (valueProperty == valueSearch)
+                        return list.ElementAt(middle);
+                    else if (valueSearch > valueProperty)
+                        start = middle + 1;
+                    else
+                        end = middle - 1;
+                } while (start <= end);
+
+                return null;
+            }
+
+            return null;
+        }
+        #endregion Algorithm Binary
+
+        #region Methods Pop for UnityTest
+        public static List<InfoList> ReturnListEmpty()
+            => new List<InfoList>();
+
+        public static List<InfoList> ReturnListInfo()
+            => new List<InfoList>() {
+                new InfoList(123, 321),
+                new InfoList(321, 1234),
+                new InfoList(2, 3212351),
+                new InfoList(145, 321241),
+                new InfoList(1238748123, 3123421),
+                new InfoList(1233213, 34321),
+                new InfoList(2, 32321),
+                new InfoList(3, 313421),
+                new InfoList(0, 23321),
+                new InfoList(123, 31321)
+            };
+        #endregion Methods Pop for UnityTest
+    }
+
+    public class InfoList
+    {
+        public InfoList(int id, int old)
+            => (Id, Old) = (id, old);
+
+        public int Id { get; set; }
+        public int Old { get; set; }
+    }
+}
